2bd07f21e23bbc6832ba4d652997f1139119c18b
lailaebrahim <ebrahimlaila815@gmail.com>
2023-12-07 18:15:04 +0200

    add show comamnd

--- console.py
+++ console.py
@@ -50,7 +50,7 @@ class HBNBCommand(cmd.Cmd):
             if args[0] not in storage.classes():
                 print("** class doesn't exist **")
             else:
-                if args[0] == "" or args(len) < 2:
+                if args(len) < 2:
                     print("** instance id missing **")
                 else:
                     key = f"{args[0]}.{args[1]}"
