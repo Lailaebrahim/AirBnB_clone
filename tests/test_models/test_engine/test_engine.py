61200cb29fd6abf308d2bab5bfc87b26879f1a26
lailaebrahim <ebrahimlaila815@gmail.com>
2023-12-10 16:07:18 +0200

    Fix

--- tests/test_models/test_engine/test_engine.py
+++ tests/test_models/test_engine/test_engine.py
@@ -20,7 +20,11 @@ class TestAmenityModel(unittest.TestCase):
 
     def setUp(self):
         """Ensure storage is empty before each test"""
-        storage._FileStorage__objects = {}
+        del_list = []
+        for key in storage._FileStorage__objects.keys():
+            del_list.append( key )
+        for key in del_list:
+            del storage._FileStorage__objects[key]
 
     def tearDown(self):
         """Clean up storage after each test"""
@@ -29,7 +33,7 @@ class TestAmenityModel(unittest.TestCase):
 
     def test_objects_dict_empty(self):
         """ __objects is initially empty """
-        self.assertEqual(len(storage.all()), 3)
+        self.assertEqual(len(storage.all()), 0)
 
     def test_instantiation_without_args(self):
         """Test creation with no arguments."""
